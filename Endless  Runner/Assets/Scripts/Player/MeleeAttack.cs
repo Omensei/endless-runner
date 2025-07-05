769580ed88896b54aed01fc5fc342555fa46dee1
Omensei <omi.abdel@gmail.com>
2025-07-05 15:35:26 -0400

    added some stuff

---
+++ Endless  Runner/Assets/Scripts/Player/MeleeAttack.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeAttack : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private Transform attackPoint;
+    [SerializeField] private float attackRange = 1f;
+    [SerializeField] private float attackDamage = 1f;
+    [SerializeField] private float attackSpeed = 1f;
+    [SerializeField] private LayerMask enemyLayers;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Attack();
+        }
+    }
+
+    private void Attack()
+    {
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<EnemyHealth>()?.TakeDamage(attackDamage);
+
+        }
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+        {
+            return;
+        }
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+}
